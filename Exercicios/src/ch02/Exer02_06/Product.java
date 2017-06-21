8fcdf8a69d7646b27a4be466c76dfc3136767a34
wendhio <wendhio.wendhio@hotmail.com>
2017-06-21 09:11:43 -0300

    Respondendo Exer02_06
    resposta correta

--- Exercicios/src/ch02/Exer02_06/Product.java
+++ Exercicios/src/ch02/Exer02_06/Product.java
@@ -5,10 +5,27 @@
 
 package ch02.Exer02_06;
 
+import java.util.Scanner;
+
 public class Product
 {
-	public static void main(String[] args)
-	{
-
-	}
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner( System.in );
+        
+        int x, y, z, result;
+        
+        System.out.println("Digite o 1 número: ");
+        x =  input.nextInt();
+        
+        System.out.println("Digite o 2 número: ");
+        y =  input.nextInt();
+        
+        System.out.println("Digite o 3 número: ");
+        z = input.nextInt();
+        
+        result = x + y + z;
+        
+        System.out.println("Product is: " + result);
+    }
 }
