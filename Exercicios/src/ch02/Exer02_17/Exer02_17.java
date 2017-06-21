aac2198a8dd2802df1a8286bde86a3db1fe2ad6f
wendhio <wendhio.wendhio@hotmail.com>
2017-06-21 12:23:52 -0300

    resposta exer02_17

--- Exercicios/src/ch02/Exer02_17/Exer02_17.java
+++ Exercicios/src/ch02/Exer02_17/Exer02_17.java
@@ -8,10 +8,58 @@
 
 package ch02.Exer02_17;
 
+import java.util.Scanner;
+
 public class Exer02_17
 {
-	public static void main(String[] args)
-	{
-		
-	}
+    public static void main(String[] args)
+    {
+        int n1, n2, n3, soma, produto;
+        double media = 0.0;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Digite o 1 número: ");
+        n1 = input.nextInt();
+
+        System.out.println("Digite o 2 número: ");
+        n2 = input.nextInt();
+        
+        System.out.println("Digite o 3 número: ");
+        n3 = input.nextInt();
+        
+        soma = n1+n2+n3;
+        produto = n1*n2*n3;
+        media = soma/3;
+        
+        System.out.println("Soma: " + soma);
+        System.out.println("Media: " + media);
+        System.out.println("Produto: " + produto);
+
+        System.out.println("Comparações: ");
+        if (n1 > n2) {
+            System.out.println("n1 é maior que n2.");
+        } else if (n1 < n2) {
+            System.out.println("n1 é menor que n2.");
+        } else {
+            System.out.println("n1 é igual a n2.");
+        }
+
+        if (n1 > n3) {
+            System.out.println("n1 é maior que n3.");
+        } else if (n1 < n3) {
+            System.out.println("n1 é menor que n3.");
+        } else {
+            System.out.println("n1 é igual a n3.");
+        }
+
+        if (n2 > n3) {
+            System.out.println("n2 é maior que n3.");
+        } else if (n2 < n3) {
+            System.out.println("n2 é menor que n3.");
+        } else {
+            System.out.println("n2 é igual a n3.");
+        }
+
+    }
 }
