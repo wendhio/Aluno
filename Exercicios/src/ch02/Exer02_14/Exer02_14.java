f36f0662bacc87ccb3a3302136f312735f80ed07
wendhio <wendhio.wendhio@hotmail.com>
2017-06-21 09:20:59 -0300

    resposta Exer02_14
    correta

--- Exercicios/src/ch02/Exer02_14/Exer02_14.java
+++ Exercicios/src/ch02/Exer02_14/Exer02_14.java
@@ -13,6 +13,8 @@ public class Exer02_14
 {
 	public static void main(String[] args)
 	{
-		
+            System.out.println("1 2 3 4");
+            System.out.print("1 2 3 4\n");
+            System.out.printf("1 2 3 4\n");
 	}
 }
