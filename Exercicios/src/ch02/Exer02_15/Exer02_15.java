280bec096af42fbfc7a1163a1e431547a1e4cb7f
wendhio <wendhio.wendhio@hotmail.com>
2017-06-21 10:07:19 -0300

    resposta Exer02_15
    correta

--- Exercicios/src/ch02/Exer02_15/Exer02_15.java
+++ Exercicios/src/ch02/Exer02_15/Exer02_15.java
@@ -6,10 +6,31 @@
 
 package ch02.Exer02_15;
 
+import java.util.Scanner;
+
 public class Exer02_15
 {
 	public static void main(String[] args)
 	{
-		
+            int n1, n2, soma, dif, div, product;
+            
+            Scanner input = new Scanner( System.in );
+            
+            System.out.println("Digite o 1 numero: ");
+            n1 = input.nextInt();
+            
+            System.out.println("Digite o 1 numero: ");
+            n2 = input.nextInt();
+            
+            soma = n1+n2;
+            dif = n1-n2;
+            product = n1*n2;
+            div = n1/n2;
+            
+            System.out.printf("Soma: %d\nDiferenca: %d\nProduto: %d\n Divisão: %d\n",
+                    soma, dif, product, div);
+            
+            
+            
 	}
 }
