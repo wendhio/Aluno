9c6f61b2101facd6be8b9990051228107af775c6
wendhio <wendhio.wendhio@hotmail.com>
2017-06-21 10:23:19 -0300

    resposta exer02_16
    correta

--- Exercicios/src/ch02/Exer02_16/Exer02_16.java
+++ Exercicios/src/ch02/Exer02_16/Exer02_16.java
@@ -7,10 +7,27 @@
 
 package ch02.Exer02_16;
 
+import java.util.Scanner;
+
 public class Exer02_16
 {
 	public static void main(String[] args)
 	{
-		
+            int number1, number2;
+            
+            Scanner input = new Scanner(System.in);
+            
+            System.out.println("Digite o 1 número: ");
+            number1 = input.nextInt();
+            
+            System.out.println("Digite o 2 número: ");
+            number2 = input.nextInt();
+            
+            if ( number1 == number2 )
+                System.out.println("São iguais!");
+            else if ( number1 > number2)
+                System.out.println("Número 1 é o maior!");
+            else
+                System.out.println("Número 2 é o maior!");
 	}
 }
