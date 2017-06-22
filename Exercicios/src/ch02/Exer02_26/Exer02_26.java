6641e85845e0b434643402967064d0a5d3766fe9
wendhio <wendhio.wendhio@hotmail.com>
2017-06-22 12:02:38 -0300

    resposta exer02_26

--- Exercicios/src/ch02/Exer02_26/Exer02_26.java
+++ Exercicios/src/ch02/Exer02_26/Exer02_26.java
@@ -6,10 +6,25 @@
 
 package ch02.Exer02_26;
 
+import java.util.Scanner;
+
 public class Exer02_26
 {
-	public static void main(String[] args)
-	{
-		
-	}
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        int n1, n2;
+        
+        System.out.println("Digite o primeiro número");
+        n1 = input.nextInt();
+        
+        System.out.println("Digite o segundo número");
+        n2 = input.nextInt();
+        
+        if (n1 % n2 == 0)
+            System.out.println("Sim!");
+        else
+            System.out.println("Não!");
+    }
 }
