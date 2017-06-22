56679f6c616f9f089b3e4d0b48bc390fe38ea6fe
wendhio <wendhio.wendhio@hotmail.com>
2017-06-22 12:57:15 -0300

    resposta exer02_32

--- Exercicios/src/ch02/Exer02_32/Exer02_32.java
+++ Exercicios/src/ch02/Exer02_32/Exer02_32.java
@@ -1,14 +1,70 @@
 /*
- * 		Escreva um programa que insere cinco números e determina e imprime quantos números
+ *  Escreva um programa que insere cinco números e determina e imprime quantos números
  * negativos, quantos número positivos e quantos zeros foram inseridos.
  */
 
 package ch02.Exer02_32;
 
+import java.util.Scanner;
+
 public class Exer02_32
 {
-	public static void main(String[] args)
-	{
-		
-	}
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+    
+        int n = 0, pos = 0, neg = 0, zeros = 0;
+
+        System.out.print("Digite o primeiro número:");
+        n = input.nextInt();
+        if (n > 0) {
+            pos++;
+        } else if (n < 0) {
+            neg++;
+        } else {
+            zeros++;
+        }
+
+        System.out.print("Digite o segundo numero:");
+        n = input.nextInt();
+        if (n > 0) {
+            pos++;
+        } else if (n < 0) {
+            neg++;
+        } else {
+            zeros++;
+        }
+
+        System.out.print("Digite o terceiro numero:");
+        n = input.nextInt();
+        if (n > 0) {
+            pos++;
+        } else if (n < 0) {
+            neg++;
+        } else {
+            zeros++;
+        }
+
+        System.out.print("Digite o quarto numero:");
+        n = input.nextInt();
+        if (n > 0) {
+            pos++;
+        } else if (n < 0) {
+            neg++;
+        } else {
+            zeros++;
+        }
+
+        System.out.print("Digite o quinto numero:");
+        n = input.nextInt();
+        if (n > 0) {
+            pos++;
+        } else if (n < 0) {
+            neg++;
+        } else {
+            zeros++;
+        }
+
+        System.out.printf("Positivos: %d, Negativos: %d, Zeros: %d", pos, neg, zeros);
+    }
 }
