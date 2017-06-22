89ae7b5a1d23dfb8543b0d405895cab8d98cab55
wendhio <wendhio.wendhio@hotmail.com>
2017-06-22 11:43:53 -0300

    resposta exer02_24

--- Exercicios/src/ch02/Exer02_24/Exer02_24.java
+++ Exercicios/src/ch02/Exer02_24/Exer02_24.java
@@ -6,10 +6,45 @@
 
 package ch02.Exer02_24;
 
+import java.util.Scanner;
+
 public class Exer02_24
 {
-	public static void main(String[] args)
-	{
-		
-	}
+    public static void main(String[] args)	
+    {
+        Scanner input = new Scanner(System.in);
+        int n1, n2, n3, n4, n5, maior;
+
+        System.out.print("Digite o primeiro inteiro:");
+        n1 = input.nextInt();
+
+        System.out.print("Digite o segundo inteiro:");
+        n2 = input.nextInt();
+
+        System.out.print("Digite o terceiro inteiro:");
+        n3 = input.nextInt();
+
+        System.out.print("Digite o quarto inteiro:");
+        n4 = input.nextInt();
+
+        System.out.print("Digite o quinto inteiro:");
+        n5 = input.nextInt();
+
+        maior = n1;
+
+        if (n2 > maior) {
+            maior = n2;
+        }
+        if (n3 > maior) {
+            maior = n3;
+        }
+        if (n4 > maior) {
+            maior = n4;
+        }
+        if (n5 > maior) {
+            maior = n5;
+        }
+
+        System.out.println("O maior numero digitado foi: " + maior);
+    }
 }
