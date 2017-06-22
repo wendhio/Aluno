c7509e74e297a80cc00197ed12f1860b0a431f86
wendhio <wendhio.wendhio@hotmail.com>
2017-06-22 18:14:06 -0300

    testes executados com sucesso! Faltando apenas uma condição de print caso o o saque fosse maior que o saldo.

--- Exercicios/src/ch03/Exer03_12/AccountTest.java
+++ Exercicios/src/ch03/Exer03_12/AccountTest.java
@@ -1,9 +1,39 @@
 package ch03.Exer03_12;
 
-public class AccountTest
+import java.util.Scanner;
+
+public class AccountTest 
 {
-	public static void main(String[] args)
-	{
-		
-	}
+    public static void main(String[] args){
+        Account conta1 = new Account( 50.00 );
+        Account conta2 = new Account( -10.00 );
+        
+        System.out.printf("Saldo conta1 = $%.2f\n", conta1.getBalance());
+        System.out.printf("Saldo conta2 = $%.2f\n", conta2.getBalance());
+        
+        Scanner input = new Scanner( System.in );
+        double depositAmount, saque;
+    
+        System.out.println("Valor de deposito conta1: ");
+        depositAmount = input.nextDouble();
+        System.out.printf("Valor depositado = %.2f\n", depositAmount);
+        conta1.credit(depositAmount); 
+     
+        System.out.printf("Saldo conta1 = $%.2f\n", conta1.getBalance());
+        System.out.printf("Saldo conta2 = $%.2f\n", conta2.getBalance());
+        
+        System.out.println("Valor de deposito conta2: ");
+        depositAmount = input.nextDouble();
+        System.out.printf("Valor depositado = %.2f\n", depositAmount);
+        conta2.credit(depositAmount);
+        
+        System.out.printf("Saldo conta1 = $%.2f\n", conta1.getBalance());
+        System.out.printf("Saldo conta2 = $%.2f\n", conta2.getBalance());
+        
+        System.out.println("Digite o valor do saque: ");
+        saque = input.nextDouble();
+        System.out.printf("Valor sacado = %.2f\n", saque);
+        conta1.debit(saque);
+        
+    }
 }
