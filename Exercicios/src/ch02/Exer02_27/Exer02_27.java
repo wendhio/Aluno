dace141a52b81a567e018edf8dbe70439575b828
wendhio <wendhio.wendhio@hotmail.com>
2017-06-22 12:08:52 -0300

    resposta exer02_27

--- Exercicios/src/ch02/Exer02_27/Exer02_27.java
+++ Exercicios/src/ch02/Exer02_27/Exer02_27.java
@@ -15,8 +15,18 @@ package ch02.Exer02_27;
 
 public class Exer02_27
 {
-	public static void main(String[] args)
-	{
-		
-	}
+    public static void main(String[] args)
+    {
+        String str = "* * * * * * * *";
+        
+	System.out.printf("%s\n", str);
+	System.out.printf(" %s\n", str);
+	System.out.printf("%s\n", str);
+	System.out.printf(" %s\n", str);
+	System.out.printf("%s\n", str);
+	System.out.printf(" %s\n", str);
+	System.out.printf("%s\n", str);
+	System.out.printf(" %s\n", str);
+        
+    }
 }
