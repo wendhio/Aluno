1cafa0016d6e0a3b3339a347f124d896e2ed2b1b
wendhio <wendhio.wendhio@hotmail.com>
2017-06-22 00:19:26 -0300

    resposta exer02_25

--- Exercicios/src/ch02/Exer02_25/Exer02_25.java
+++ Exercicios/src/ch02/Exer02_25/Exer02_25.java
@@ -6,10 +6,20 @@
 
 package ch02.Exer02_25;
 
+import java.util.Scanner;
+
 public class Exer02_25
 {
-	public static void main(String[] args)
-	{
-		
-	}
+    public static void main(String[] args)	
+    {
+        int number;
+        
+        Scanner input = new Scanner(System.in);
+        number = input.nextInt();
+        
+        if ( number % 2 == 0)
+            System.out.println("Par");
+        else    
+            System.out.println("Ímpar");
+    }
 }
