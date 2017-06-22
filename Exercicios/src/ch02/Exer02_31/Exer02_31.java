0b04c20d6e901e37a2f1288c82488c8af8cda13f
wendhio <wendhio.wendhio@hotmail.com>
2017-06-22 12:51:45 -0300

    resposta exer02_30

--- Exercicios/src/ch02/Exer02_31/Exer02_31.java
+++ Exercicios/src/ch02/Exer02_31/Exer02_31.java
@@ -20,8 +20,19 @@ package ch02.Exer02_31;
 
 public class Exer02_31
 {
-	public static void main(String[] args)
-	{
-		
-	}
+    public static void main(String[] args)
+    {
+        System.out.println("número quadrado cubo");
+        System.out.printf("%d      %s      %s\n", 0, Math.pow(0, 2), Math.pow(0, 3));
+        System.out.printf("%d      %s      %s\n", 1, Math.pow(1, 2), Math.pow(1, 3));
+        System.out.printf("%d      %s      %s\n", 2, Math.pow(2, 2), Math.pow(2, 3));
+        System.out.printf("%d      %s      %s\n", 3, Math.pow(3, 2), Math.pow(3, 3));
+        System.out.printf("%d      %s     %s\n", 4, Math.pow(4, 2), Math.pow(4, 3));
+        System.out.printf("%d      %s     %s\n", 5, Math.pow(5, 2), Math.pow(5, 3));
+        System.out.printf("%d      %s     %s\n", 6, Math.pow(6, 2), Math.pow(6, 3));
+        System.out.printf("%d      %s     %s\n", 7, Math.pow(7, 2), Math.pow(7, 3));
+        System.out.printf("%d      %s     %s\n", 8, Math.pow(8, 2), Math.pow(8, 3));
+        System.out.printf("%d      %s     %s\n", 9, Math.pow(9, 2), Math.pow(9, 3));
+        System.out.printf("%d     %s    %s\n", 10, Math.pow(10, 2), Math.pow(10, 3));
+    }
 }
