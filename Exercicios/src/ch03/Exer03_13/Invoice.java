1807334c74bbe47b262b9d21410eacec5231dd2f
wendhio <wendhio.wendhio@hotmail.com>
2017-07-18 21:23:32 -0300

    testes executados com sucesso! Faltando apenas uma condição de print caso o o saque fosse maior que o saldo.

--- Exercicios/src/ch03/Exer03_13/Invoice.java
+++ Exercicios/src/ch03/Exer03_13/Invoice.java
@@ -16,5 +16,60 @@ package ch03.Exer03_13;
 
 public class Invoice
 {
-	
-}
+    private String num, des;	//Número e descrição
+    private int quant;		//Quantidade
+    private double preco;	//Preço
+
+    public Invoice(String num, String des, int quant, double preco) {
+        super();
+        setNum(num);
+        setDes(des);
+        setQuant(quant);
+        setPreco(preco);
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        if (preco < 0) {
+            this.preco = 0;
+        } else {
+            this.preco = preco;
+        }
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        if (quant < 0) {
+            this.quant = 0;
+        } else {
+            this.quant = quant;
+        }
+    }
+
+    public double getInvoiceAmount() {
+        return quant * preco;
+    }
+}   
+    
