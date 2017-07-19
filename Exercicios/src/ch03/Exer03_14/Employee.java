df3b85403fd9f7217c4e8609913653e59c7d4729
wendhio <wendhio.wendhio@hotmail.com>
2017-07-19 02:14:52 -0300

    ok! obs.: não usado o sobrenome.

--- Exercicios/src/ch03/Exer03_14/Employee.java
+++ Exercicios/src/ch03/Exer03_14/Employee.java
@@ -9,7 +9,51 @@
 
 package ch03.Exer03_14;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 public class Employee
 {
+    private String nome, sobrenome;
+    private double salario;
+    
+    public void Employee( String nome, String sobrenome, double salario){
+        setNome(nome);
+        setSobrenome(sobrenome);
+        setSalario(salario);
+    }
+    
+    public void setNome( String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){ return nome;}
+    
+    public void setSobrenome( String sobrenome ){
+        this.sobrenome = sobrenome;
+    }
+    
+    public String getSobrenome(){ return sobrenome;}
+    
+    public void setSalario( double salario){
+        if ( salario < 0)
+            this.salario = 0.0;
+        else
+            this.salario = salario;
+    }
+    
+    public double getSalario(){ return salario; }
+    
+    public void entrada(){
+        Scanner scan = new Scanner( System.in );
+        
+        String nome = JOptionPane.showInputDialog("Digite seu nome:");
+        this.nome = nome;
+        
+        double salario = Double.parseDouble(JOptionPane.showInputDialog("Quanto recebe?")); 
+        this.salario = salario;
+        
+        
+    }
 
 }
