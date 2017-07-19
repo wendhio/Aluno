cc0e788d27bceeb2623140344bd15c9175467bb7
wendhio <wendhio.wendhio@hotmail.com>
2017-07-18 21:02:56 -0300

    testes executados com sucesso! Faltando apenas uma condição de print caso o o saque fosse maior que o saldo.

--- Exercicios/src/ch03/Exer03_13/InvoiceTest.java
+++ Exercicios/src/ch03/Exer03_13/InvoiceTest.java
@@ -2,8 +2,21 @@ package ch03.Exer03_13;
 
 public class InvoiceTest
 {
-	public static void main(String[] args)
-	{
-		
-	}
+    public static void main(String[] args)
+    {
+        Invoice i = new Invoice("011.527.080-90", "Informática",
+                500, 199.90);
+
+        System.out.printf("Numero: %s\nDescrição: %s\nQuantidade: %d\n"
+                + "Preço: %s\nTotal: %s\n", i.getNum(), i.getDes(),
+                i.getQuant(), i.getPreco(), i.getInvoiceAmount());
+
+        i.setPreco(100.00);
+        i.setQuant(3);
+
+        System.out.println("-;-;-;-;-;-;-;-;-;-;-;-;-;-;-;-;-;-;-;-;-;");
+        System.out.printf("Numero: %s\nDescrição: %s\nQuantidade: %d\n"
+                + "Preço: %s\nTotal: %s\n", i.getNum(), i.getDes(),
+                i.getQuant(), i.getPreco(), i.getInvoiceAmount());
+    }
 }
