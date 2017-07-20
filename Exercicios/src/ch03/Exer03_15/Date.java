b744ba1c7dfa9f8684ef8d111ecd94bdad0e6e32
wendhio <wendhio.wendhio@hotmail.com>
2017-07-20 01:04:22 -0300

    certo!

--- Exercicios/src/ch03/Exer03_15/Date.java
+++ Exercicios/src/ch03/Exer03_15/Date.java
@@ -9,7 +9,35 @@
 
 package ch03.Exer03_15;
 
-public class Date
-{
+public class Date{
+    
+    int dia, mes, ano;
+    
+    public Date( int dia, int mes, int ano){
+        setDia(dia);
+        setMes(mes);
+        setAno(ano);
+    }
+    
+    public void setDia( int dia){
+        this.dia = dia;
+    }
+    
+    public int getDia() { return dia; }
 
+    public int getMes() { return mes; }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() { return ano; }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    
+    public void imprimeData(){
+        System.out.printf("%d/%d/%d", getDia(), getMes(), getAno());
+    }
 }
