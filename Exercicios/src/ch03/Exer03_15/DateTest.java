e0c169429ac8fca44f300fee2f861c23324778e9
wendhio <wendhio.wendhio@hotmail.com>
2017-07-20 01:04:57 -0300

    aprovado!

--- Exercicios/src/ch03/Exer03_15/DateTest.java
+++ Exercicios/src/ch03/Exer03_15/DateTest.java
@@ -3,6 +3,9 @@ package ch03.Exer03_15;
 public class DateTest {
 	public static void main(String[] args)
 	{
-		
+            Date data = new Date( 20, 07, 2017 );
+            
+            
+            data.imprimeData();
 	}
 }
