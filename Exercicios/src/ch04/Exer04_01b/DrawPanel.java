6dd854258e6c2ea3a7eb4c7db6c7d818d4e8467a
wendhio <wendhio.wendhio@hotmail.com>
2017-08-01 21:33:36 -0300

    ok!

--- Exercicios/src/ch04/Exer04_01b/DrawPanel.java
+++ Exercicios/src/ch04/Exer04_01b/DrawPanel.java
@@ -20,7 +20,96 @@
 
 package ch04.Exer04_01b;
 
-public class DrawPanel
-{
+import javax.swing.JPanel;
+import java.awt.Graphics;
 
+public class DrawPanel extends JPanel{
+    
+    public void paintComponent( Graphics g ){
+
+        super.paintComponent(g);
+
+        int width = getWidth() / 2; // largura total
+        int height = getHeight() / 2; // altura total
+        g.drawLine(0, 0, width, height);
+
+        for (int i = 1; i <= 7; i++) {
+
+            width = width + getWidth() / 15;
+            height = height - getHeight() / 15;
+
+            g.drawLine(0, 0, width, height);
+
+        }
+
+        width = getWidth() / 2; // largura total
+        height = getHeight() / 2; // altura total
+        for (int i = 1; i <= 7; i++) {
+            width = width - getWidth() / 15;
+            height = height + getHeight() / 15;
+
+            g.drawLine(0, 0, width, height);
+        }
+
+        width = getWidth() / 2; // largura total
+        height = getHeight() / 2; // altura total
+        g.drawLine(getWidth(), getHeight(), width, height);
+
+        for (int i = 1; i <= 7; i++) {
+            width = width + getWidth() / 15;
+            height = height - getHeight() / 15;
+            g.drawLine(getWidth(), getHeight(), width, height);
+        }
+
+        width = getWidth() / 2; // largura total
+        height = getHeight() / 2; // altura total
+        for (int i = 1; i <= 7; i++) {
+
+            width = width - getWidth() / 15;
+            height = height + getHeight() / 15;
+
+            g.drawLine(getWidth(), getHeight(), width, height);
+
+        }
+
+        width = getWidth() / 2; // largura total
+        height = getHeight() / 2; // altura total
+        g.drawLine(0, getHeight(), width, height);
+
+        for (int i = 1; i <= 7; i++) {
+            width = width + getWidth() / 15;
+            height = height + getHeight() / 15;
+
+            g.drawLine(0, getHeight(), width, height);
+        }
+
+        width = getWidth() / 2; // largura total
+        height = getHeight() / 2; // altura total
+        for (int i = 1; i <= 7; i++) {
+            width = width - getWidth() / 15;
+            height = height - getHeight() / 15;
+
+            g.drawLine(0, getHeight(), width, height);
+        }
+
+        width = getWidth() / 2; // largura total
+        height = getHeight() / 2; // altura total
+        g.drawLine(getWidth(), 0, width, height);
+
+        for (int i = 1; i <= 7; i++) {
+            width = width + getWidth() / 15;
+            height = height + getHeight() / 15;
+
+            g.drawLine(getWidth(), 0, width, height);
+        }
+
+        width = getWidth() / 2; // largura total
+        height = getHeight() / 2; // altura total
+        for (int i = 1; i <= 7; i++) {
+            width = width - getWidth() / 15;
+            height = height - getHeight() / 15;
+
+            g.drawLine(getWidth(), 0, width, height);
+        }
+    }
 }
