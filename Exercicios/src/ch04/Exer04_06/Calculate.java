082ad3912cd64d6dfa95188d397d9aba982b071c
wendhio <wendhio.wendhio@hotmail.com>
2017-08-01 22:23:18 -0300

    respondido.

--- Exercicios/src/ch04/Exer04_06/Calculate.java
+++ Exercicios/src/ch04/Exer04_06/Calculate.java
@@ -7,10 +7,17 @@
 
 package ch04.Exer04_06;
 
-public class Calculate
-{
-	public static void main(String[] args)
-	{
-		
-	}
+public class Calculate{
+    public static void main(String[] args){
+        int sum, x;
+
+        x = 1;
+        sum = 0;
+
+        while (x <= 10) {
+            sum += x++;
+            //++x;
+        }
+        System.out.println("A soma é: " + sum);
+    }
 }
